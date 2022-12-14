e408ce054321d5862acba4fddaeb5fa8e49ed462
Eric Pereira <92660469+EricPereira234@users.noreply.github.com>
2022-12-14 07:51:51 -0300

    Initial commit

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCyRhmgleXKW6HnFqaMgwy3vTLS3jMRWFM",
+  authDomain: "sistema-chamados-36486.firebaseapp.com",
+  projectId: "sistema-chamados-36486",
+  storageBucket: "sistema-chamados-36486.appspot.com",
+  messagingSenderId: "732187291935",
+  appId: "1:732187291935:web:d194445d0118e688af802a",
+  measurementId: "G-RV6Q70P7YT"
+};
+
+if(!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
