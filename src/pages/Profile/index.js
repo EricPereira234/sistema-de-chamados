bbb14af55359e6b9e9d8a8302f60a21b5fb97492
Eric Pereira <92660469+EricPereira234@users.noreply.github.com>
2022-12-19 20:01:19 -0300

    perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -22,9 +22,57 @@ export default function Profile() {
     const [avatarUrl, setAvatarUrl] = useState(user && user.avaterUrl);
     const [imageAvatar, setImageAvatar] = useState(null);
 
+
+    //previl da imagem 
+    function handleFile(e){
+        if(e.target.files[0]){
+            const image = e.target.files[0];
+
+            if(image.type === 'image/jpeg' || image.type === 'image/png'){
+                setImageAvatar(image);
+                setAvatarUrl(URL.createObjectURL(e.target.files[0]));
+            }else{
+                toast.error('envie uma imagem do tipo jpeg ou PNG');
+                setImageAvatar(null);
+                return null;
+            }
+        }
+    }
+
     //salvando imagem do perfil
-    function handleUpload(){
-        
+     async function handleUpload(){
+        const currentUid = user.uid;
+
+        const uploadTask = await firebase.storage()
+        .ref(`images/${currentUid}/${imageAvatar.name}`)
+        .put(imageAvatar)
+        .then( async ()=>{
+            toast.success('imagem enviada com sucesso!');
+
+            await firebase.storage().ref(`images/${currentUid}`)
+            .child(imageAvatar.name).getDownloadURL()
+            .then( async (url)=>{
+                let urlFoto = url;
+
+                await firebase.firestore().collection('users')
+                .doc(user.uid)
+                .update({
+                    avatarUrl: urlFoto,
+                    nome: nome
+                })
+                .then(()=>{
+                    let data = {
+                        ...user,
+                        avatarUrl: urlFoto,
+                        nome: nome
+                    };
+
+                    setUser(data);
+                    storageUser(data);
+                })
+
+            })
+        })
     }
 
    async function handleSave(e){
@@ -66,8 +114,8 @@ export default function Profile() {
                             <span>
                                 <FiUpload color="#fff" size={25} />
                             </span>
-                            <input type={'file'} accept="image/*" /><br/>
-                            { avatarUrl == null  ?
+                            <input type='file' accept="image/*"  onChange={handleFile} /><br/>
+                            { avatarUrl == null ?
                              <img src={avatar} width='250' height='250' alt='Foto de perfil do usurário' /> 
                              :
                              <img src={avatarUrl} width='250' height='250' alt='Foto de perfil do usurário' />
