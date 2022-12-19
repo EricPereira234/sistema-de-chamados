1d6900a9495db4e3ebcab9bb78ef73fee54b64af
Eric Pereira <92660469+EricPereira234@users.noreply.github.com>
2022-12-19 07:49:23 -0300

    perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,6 @@
 import "./profile.css";
 import { useState, useContext } from "react";
+import {toast} from "react-toastify";
 
 //importando aquivos
 import Header from "../../components/Header";
@@ -10,12 +11,46 @@ import avatar from "../../assets/avatar.png";
 //importando icones
 import { FiSettings, FiUpload } from "react-icons/fi";
 
+//importando o firebase
+import firebase from "../../services/firebaseConnection";
+
 export default function Profile() {
     const {user, signOut, setUser, storageUser} = useContext(AuthContext);
 
     const [nome, setNome] = useState(user && user.nome);
     const [email, setEmail] = useState(user && user.email);
     const [avatarUrl, setAvatarUrl] = useState(user && user.avaterUrl);
+    const [imageAvatar, setImageAvatar] = useState(null);
+
+    //salvando imagem do perfil
+    function handleUpload(){
+        
+    }
+
+   async function handleSave(e){
+        e.preventDefault();
+
+        if(imageAvatar == null && nome !== ''){
+            await firebase.firestore().collection('users')
+            .doc(user.uid)
+            .update({
+                nome: nome
+            }).then(()=>{
+                let data = {
+                    ...user,
+                    nome: nome
+                };
+                setUser(data);
+                storageUser(data);
+                toast.success('alterado com sucesso!');
+            })
+        } else if(nome !== '' && imageAvatar !== null){
+            handleUpload();
+        }
+
+    }
+
+
     return (
         <div>
             <Header />
@@ -26,7 +61,7 @@ export default function Profile() {
                 </Tilte>
 
                 <div className="container" >
-                    <form className="form-profile" >
+                    <form className="form-profile" onSubmit={handleSave} >
                         <label className="label-avatar" >
                             <span>
                                 <FiUpload color="#fff" size={25} />
