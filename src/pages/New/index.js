493fda326036dce98258ef80c676662acdd893a9
Eric Pereira <92660469+EricPereira234@users.noreply.github.com>
2023-01-09 20:01:24 -0300

    chamados

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,211 +1,245 @@
-import "./new.css";
-import { useState, useEffect, useContext } from "react";
-import { useHistory, useParams } from "react-router-dom";
-import { toast } from "react-toastify";
 
-//importando o firebase
-import firebase from "../../services/firebaseConnection";
+import { useState, useEffect, useContext } from 'react';
 
-//importando arquivos
-import Header from "../../components/Header";
-import Title from "../../components/Title";
-import { AuthContext } from "../../contexts/auth";
+import firebase from '../../services/firebaseConnection';
+import { useHistory, useParams } from 'react-router-dom';
 
-//importando icones
-import { FiPlus } from "react-icons/fi";
+import Header from '../../components/Header';
+import Title from '../../components/Title';
+import { AuthContext } from '../../contexts/auth';
+import { toast } from 'react-toastify';
 
-export default function New() {
+import './new.css';
+import { FiPlusCircle } from 'react-icons/fi'
 
-    const {id} = useParams();
-    const history = useHistory(0);
+export default function New(){
+  const { id } = useParams();
+  const history = useHistory();
 
-    const [assunto, setAssunto] = useState('Suporte');
-    const [status, setStatus] = useState('Aberto');
-    const [complemento, setComplemento] = useState('');
+  const [loadCustomers, setLoadCustomers] = useState(true);
+  const [customers, setCustomers] = useState([]);
+  const [customerSelected, setCustomerSelected] = useState(0);
 
-    const { user } = useContext(AuthContext);
-    const [loadCustomers, setLoadCustomers] = useState(true);
-    const [customers, setCustomers] = useState([]);
-    const [customersSelected, setCustomersSelected] = useState(0);
-    
-    const [idCustomer, setIdCoustomer] = useState(false);
+  const [assunto, setAssunto] = useState('Suporte');
+  const [status, setStatus] = useState('Aberto');
+  const [complemento, setComplemento] = useState('');
 
-    useEffect(() => {
-        async function loadCustomers() {
-            await firebase.firestore().collection('customers')
-                .get()
-                .then((snapshot) => {
-                    let lista = [];
+  const [idCustomer, setIdCustomer] = useState(false);
 
-                    snapshot.forEach((doc) => {
-                        lista.push({
-                            id: doc.id,
-                            nomeFantasia: doc.data().nomeFantasia
-                        })
-                    })
+  const { user } = useContext(AuthContext);
 
-                    if (lista.length === 0) {
-                        toast.error('não existe empresa cadastrada !');
-                        setLoadCustomers(false);
-                        return;
-                    }
 
-                    setCustomers(lista);
-                    setLoadCustomers(false);
+  useEffect(()=> {
+    async function loadCustomers(){
+      await firebase.firestore().collection('customers')
+      .get()
+      .then((snapshot)=>{
+        let lista = [];
 
-                    //se tiver clientes 
-                    if(id){
-                        loadId(lista);
-                    }
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            nomeFantasia: doc.data().nomeFantasia
+          })
+        })
 
-                })
-                .catch((error) => {
-                    setLoadCustomers(false);
-                   
-                })
-
+        if(lista.length === 0){
+          console.log('NENHUMA EMPRESA ENCONTRADA');
+          setCustomers([ { id: '1', nomeFantasia: 'FREELA' } ]);
+          setLoadCustomers(false);
+          return;
         }
 
-        loadCustomers()
-    }, []);
+        setCustomers(lista);
+        setLoadCustomers(false);
 
+        if(id){
+          loadId(lista);
+        }
 
-    //função que lista os dados dos chamados para edição
-    async function loadId(lista){
-        await firebase.firestore().collection('chamados').doc(id)
-        .get()
-        .then((snapshot)=>{
-            setAssunto(snapshot.data().assunto);
-            setStatus(snapshot.data().status);
-            setComplemento(snapshot.data().complemento);
+      })
+      .catch((error)=>{
+        console.log('DEU ALGUM ERRO!', error);
+        setLoadCustomers(false);
+        setCustomers([ { id: '1', nomeFantasia: '' } ]);
+      })
 
-            let index = lista.findIndex(item => item.id === snapshot.data().clienteId);
-            setCustomersSelected(index);
-            setIdCoustomer(true);
-        })
-        .catch((erro)=>{
-            setIdCoustomer(false);
-        })
     }
 
+    loadCustomers();
 
-    // funcão que salva os chamados
-    async function hadleRegister(e) {
-        e.preventDefault();
-        await firebase.firestore().collection('chamados')
-        .add({
-            created: new Date(),
-            cliente: customers[customersSelected],
-            assunto: assunto,
-            status: status,
-            complemento: complemento,
-            userId: user.uid
-        })
-        .then(()=>{
-            toast.success('Chamado criado com sucesso!');
-            setComplemento('');
-            setCustomersSelected(0);
-        })
-        .catch((erro)=>{
-            toast.error('Erro ao cadastar chamado!');
-        })
+  }, [id]);
+
+
+
+  async function loadId(lista){
+    await firebase.firestore().collection('chamados').doc(id)
+    .get()
+    .then((snapshot) => {
+      setAssunto(snapshot.data().assunto);
+      setStatus(snapshot.data().status);
+      setComplemento(snapshot.data().complemento)
+
+      let index = lista.findIndex(item => item.id === snapshot.data().clienteId );
+      setCustomerSelected(index);
+      setIdCustomer(true);
+
+    })
+    .catch((err)=>{
+      console.log('ERRO NO ID PASSADO: ', err);
+      setIdCustomer(false);
+    })
+  }
+
+  async function handleRegister(e){
+    e.preventDefault();
+
+    if(idCustomer){
+      await firebase.firestore().collection('chamados')
+      .doc(id)
+      .update({
+        cliente: customers[customerSelected],
+        clienteId: customers[customerSelected].id,
+        assunto: assunto,
+        status: status,
+        complemento: complemento,
+        userId: user.uid
+      })
+      .then(()=>{
+        toast.success('Chamado Editado com sucesso!');
+        setCustomerSelected(0);
+        setComplemento('');
+        history.push('/dashboard');
+      })
+      .catch((err)=>{
+        toast.error('Ops erro ao registrar, tente mais tarde.')
+        console.log(err);
+      })
+
+      return;
     }
 
-    //chama quando troca o assunto
-    function handleChangeSelect(e) {
-        setAssunto(e.target.value);
-    }
-    //chama quando troca o status
-    function options(e) {
-        setStatus(e.target.value);
-    }
-
-    //chamado quando troca o cliente
-    function handleChageCustomers(e) {
-        setCustomersSelected(e.target.value);
-    }
-
-    return (
-        <>
-
-            <Header />
-            <div className="content">
-                <Title name={'Novo Chamado'} >
-                    <FiPlus size={25} />
-                </Title>
-
-                <div className="container">
-
-                    <form className="form-profile" onSubmit={hadleRegister} >
-                        <label>Cliente</label>
-                        {loadCustomers ? (
-                            <input type={'text'} disabled={true} value={'Carregando...'} />
-                        ) 
-                        :
-                            <select value={customersSelected} onChange={handleChageCustomers} >
-                                {customers.map((item, index) => {
-                                    return (
-                                        <option key={item.id} value={index}>
-                                            {item.nomeFantasia}
-                                        </option>
-                                    )
-                                })}
-                            </select>
-
-                        }
+    await firebase.firestore().collection('chamados')
+    .add({
+      created: new Date(),
+      cliente: customers[customerSelected],
+      clienteId: customers[customerSelected].id,
+      assunto: assunto,
+      status: status,
+      complemento: complemento,
+      userId: user.uid
+    })
+    .then(()=> {
+      toast.success('Chamado criado com sucesso!');
+      setComplemento('');
+      setCustomerSelected(0);
+    })
+    .catch((err)=> {
+      toast.error('Ops erro ao registrar, tente mais tarde.')
+      console.log(err);
+    })
 
 
-                        <label>Assunto</label>
-                        <select value={assunto} onChange={handleChangeSelect} >
-                            <option value='Supore'>Suporte</option>
-                            <option value='Visita Técnica'>Visita Técnica</option>
-                            <option value='Financeiro'>Financeiro</option>
-                        </select>
+  }
 
 
-                        <label>Status</label>
-                        <div className="status">
-                            <input
-                                type={'radio'}
-                                name='radio'
-                                value={'Aberto'}
-                                onChange={options}
-                                checked={status === 'Aberto'}
-                            />
-                            <span>Em Aberto</span>
+  //Chamado quando troca o assunto
+  function handleChangeSelect(e){
+    setAssunto(e.target.value);
+  }
 
-                            <input
-                                type={'radio'}
-                                name='radio'
-                                value={'Progresso'}
-                                onChange={options}
-                                checked={status === 'Progresso'}
-                            />
-                            <span>Progresso</span>
 
-                            <input
-                                type={'radio'}
-                                name='radio'
-                                value={'Atendido'}
-                                onChange={options}
-                                checked={status === 'Atendido'}
-                            />
-                            <span>Atendido</span>
-                        </div>
+  //Chamado quando troca o status
+  function handleOptionChange(e){
+    setStatus(e.target.value);
+  }
 
-                        <label>Complemento</label>
-                        <textarea
-                            type='text'
-                            placeholder='Descreva seu problema (opcional)'
-                            value={complemento}
-                            onChange={(e) => setComplemento(e.target.value)}
-                        />
+  //Chamado quando troca de cliente
+  function handleChangeCustomers(e){
+    setCustomerSelected(e.target.value);
+  }
 
-                        <button type="submit" >Salvar</button>
-                    </form>
-                </div>
+  return(
+    <div>
+      <Header/>
+
+      <div className="content">
+        <Title name="Novo chamado">
+          <FiPlusCircle size={25} />
+        </Title>
+
+        <div className="container">
+
+          <form className="form-profile"  onSubmit={handleRegister} >
+            
+            <label>Cliente</label>
+
+            {loadCustomers ? (
+              <input type="text" disabled={true} value="Carregando clientes..." />
+            ) : (
+                <select value={customerSelected} onChange={handleChangeCustomers} >
+                {customers.map((item, index) => {
+                  return(
+                    <option key={item.id} value={index} >
+                      {item.nomeFantasia}
+                    </option>
+                  )
+                })}
+              </select>
+            )}
+
+            <label>Assunto</label>
+            <select value={assunto} onChange={handleChangeSelect}>
+              <option value="Suporte">Suporte</option>
+              <option value="Visita Tecnica">Visita Tecnica</option>
+              <option value="Financeiro">Financeiro</option>
+            </select>
+
+            <label>Status</label>
+            <div className="status">
+              <input 
+              type="radio"
+              name="radio"
+              value="Aberto"
+              onChange={handleOptionChange}
+              checked={ status === 'Aberto' }
+              />
+              <span>Em Aberto</span>
+
+              <input 
+              type="radio"
+              name="radio"
+              value="Progresso"
+              onChange={handleOptionChange}
+              checked={ status === 'Progresso' }
+              />
+              <span>Progresso</span>
+
+              <input 
+              type="radio"
+              name="radio"
+              value="Atendido"
+              onChange={handleOptionChange}
+              checked={ status === 'Atendido' }
+              />
+              <span>Atendido</span>
             </div>
 
-        </>
-    )
+            <label>Complemento</label>
+            <textarea
+              type="text"
+              placeholder="Descreva seu problema (opcional)."
+              value={complemento}
+              onChange={ (e) => setComplemento(e.target.value) }
+            />
+            
+            <button type="submit">Registrar</button>
+
+          </form>
+
+        </div>
+
+      </div>
+    </div>
+  )
 }
